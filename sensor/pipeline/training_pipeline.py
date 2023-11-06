5782b53373dd71cd6475c5574235b305a8aca7db
Rohit Kumar <rohit.kr.0310@gmail.com>
2023-11-06 19:23:54 +0100

    data transformation implemented

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,7 +1,8 @@
-from sensor.entity.config_entity import TrainingPipeLineConfig, DataIngestionConfig, DataValidationConfig
-from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from sensor.entity.config_entity import TrainingPipeLineConfig, DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact
 from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
+from sensor.components.data_transformation import DataTransformation
 from sensor.exception import SensorException
 from sensor.logger import logging
 import sys, os
@@ -34,9 +35,14 @@ class TrainPipeline:
         except Exception as e:
             raise SensorException(e, sys)
         
-    def start_data_transformation(self):
+    def start_data_transformation(self, data_validation_artifact: DataValidationArtifact) -> DataTransformationArtifact:
         try:
-            pass
+            logging.info("Starting data transformation")
+            data_transformation_config = DataTransformationConfig(self.training_pipeline_config)
+            data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            logging.info(f"Data transformation completed successfully and artifact : {data_transformation_artifact}")
+            return data_transformation_artifact
         except Exception as e:
             raise SensorException(e, sys)
     
@@ -62,5 +68,6 @@ class TrainPipeline:
         try:
             data_ingestion_artifact: DataIngestionArtifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_transformation_artifact = self.start_data_transformation(data_validation_artifact)
         except Exception as e:
             raise SensorException(e, sys)
--- sensor/utils/main_utils.py
+++ sensor/utils/main_utils.py
@@ -1,6 +1,9 @@
 from sensor.exception import SensorException
+from sensor.logger import logging
 import yaml
 import sys, os
+import numpy as np
+import dill
 
 def read_yaml_file(filepath:str) -> dict:
     try:
@@ -19,3 +22,54 @@ def write_yaml_file(file_path:str, content: object, replace: bool=False) -> None
             yaml.dump(content, file)
     except Exception as e:
         raise SensorException(e, sys)
+    
+def save_numpy_array_data(file_path:str, array: np.array):
+    """
+    Save the numpy array data to file
+
+    Args:
+        file_path (str): location of file to save
+        array (np.array): data to save
+    """
+
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file:
+            np.save(file, array)
+    except Exception as e:
+        raise SensorException(e, sys)
+    
+def load_numpy_array_data(file_path:str):
+    """
+    Load the numpy array data to file
+
+    Args:
+        file_path (str): location of file to save
+    return : np.array data
+    """
+
+    try:
+        with open(file_path, 'rb') as file:
+            return np.load(file)
+    except Exception as e:
+        raise SensorException(e, sys)
+    
+def save_object(file_path: str, obj: object) -> None:
+    try:
+        logging.info("Saving object..")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+        logging.info("Object saved...")
+    except Exception as e:
+        raise SensorException(e, sys) from e
+    
+def load_object(file_path: str) -> object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file: {file_path} is not exists")
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise SensorException(e, sys) from e
